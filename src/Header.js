80f0099a359f19c6460e74993727e16f4c903dc9
r556a812 <richardaviles81@gmail.com>
2019-07-31 19:29:45 -0500

    Create Homepage

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="head">
+            Welcome to Movie and TV Search!
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Search () {
+    return(
+        <div id="search-bar">
+            <h2>Search For Your Favorite Movies and TV Shows</h2>
+            <input placeholder="Search for ..."></input>
+            <button type="submit">Search</button>
+        </div>
+    )
+}
+
+export default Search
